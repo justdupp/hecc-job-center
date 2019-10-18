ed2527c0f3f39a7c5f1d39171351d67a76f649cd
xuhoujun <hqxhj1990@163.com>
2019-10-18 13:33:13 +0800

    初始化配置

--- src/main/java/com/hecc/cloud/JobApplication.java
+++ src/main/java/com/hecc/cloud/JobApplication.java
@@ -1,13 +1,23 @@
 package com.hecc.cloud;
 
+import groovy.util.logging.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author xuhoujun
+ * 定时脚本启动类
+ */
 @SpringBootApplication
+@EnableAsync
+@Slf4j
+@EnableScheduling
 public class JobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
 
 }
