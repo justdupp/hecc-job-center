e33a4796bdf73d598148fa9a79a1b55231b2f399
xuhoujun <hqxhj1990@163.com>
2019-10-18 11:58:06 +0800

    初始化项目

---
+++ src/test/java/com/hecc/cloud/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecc.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
